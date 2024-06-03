4ccb152c2a7162f3800fec433e438326957afd69
EzraArya <ezrawijaya10@gmail.com>
2024-06-03 08:37:39 +0700

    feat: add project page

---
+++ src/app/projects/portofolio/page.tsx
@@ -0,0 +1,68 @@
+/* eslint-disable @next/next/no-img-element */
+/* eslint-disable react/no-unescaped-entities */
+import Image from "next/image";
+import Link from "next/link";
+
+
+export default function Home(){
+    return (
+        <main>
+            <nav className='flex justify-between m-14'>
+                <div>
+                    <ul className="gap-6 flex justify-start font-montserrat font-bold text-2xl drop-shadow-md">
+                        <li>
+                            <Link href="/" className='hover:animate-bounce hover:underline decoration-blue-500 decoration-3 decoration-wavy'>
+                                <h1>
+                                    Home
+                                </h1>
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+            <div className="flex flex-col-reverse gap-20 md:flex-row w-screen/2 m-14 items-center h-4/5 justify-evenly" id="hero">
+                <div className="flex flex-col gap-2 justify-center font-montserrat">
+                    <h1 className="font-semibold text-4xl">Portofolio Website</h1>
+                    <h1 className="font-light text-lg">This is a portofolio website to show all my project and to know more about myself, this is my first attempt at NextJs hoping to improve it much more</h1>
+                    <h1 className="font-semibold text-2xl">Techstack Used:</h1>
+                    <div className="flex overflow-x-auto gap-4 mt-4">
+                        <img className="w-10 h-10" src="/next.svg" alt="NextJs Logo"></img>
+                        <svg className="w-8 h-8 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                            <path d="M15.907 11.998 10.332 9.23a.9.9 0 0 1-.16-.037l-.018-.007v6.554c0 .017.008.034.01.051l2.388-2.974 3.355-.82Z"/>
+                            <path d="m11.463 4.054 5.579 3.323A4.02 4.02 0 0 1 18.525 9c.332.668.47 1.414.398 2.155a3.07 3.07 0 0 1-.745 1.65 3.108 3.108 0 0 1-1.55.951c-.022.007-.045.005-.07.01-.062.03-.126.057-.191.08l-2.72.667-1.992 2.48c-.18.227-.41.409-.67.534.047.034.085.077.137.107a2.05 2.05 0 0 0 1.995.035c.592-.33 2.15-1.201 4.636-2.892l.28-.19c1.328-.895 3.616-2.442 3.967-4.215a9.94 9.94 0 0 0-1.713-4.154 10.027 10.027 0 0 0-3.375-2.989 10.107 10.107 0 0 0-8.802-.418c1.162.287 2.287.704 3.354 1.243Z"/>
+                            <path d="M5.382 17.082v-6.457a3.7 3.7 0 0 1 .45-1.761 3.733 3.733 0 0 1 1.238-1.34 3.915 3.915 0 0 1 3.433-.245c.176.03.347.084.508.161l5.753 2.856c.082.05.161.105.236.165a2.128 2.128 0 0 0-.953-1.455l-5.51-3.284c-1.74-.857-3.906-1.523-5.244-1.097a9.96 9.96 0 0 0-2.5 3.496 9.895 9.895 0 0 0 .283 8.368 9.973 9.973 0 0 0 2.73 3.322 17.161 17.161 0 0 1-.424-2.729Z"/>
+                            <path d="m19.102 16.163-.272.183c-2.557 1.74-4.169 2.64-4.698 2.935a4.083 4.083 0 0 1-2 .53 3.946 3.946 0 0 1-1.983-.535 3.788 3.788 0 0 1-1.36-1.361 3.752 3.752 0 0 1-.51-1.85 1.812 1.812 0 0 1-.043-.26V9.143c0-.024.009-.046.01-.07-.056.02-.11.043-.162.07a1.796 1.796 0 0 0-.787 1.516v6.377a10.67 10.67 0 0 0 1.113 4.27 10.11 10.11 0 0 0 8.505-.53 10.022 10.022 0 0 0 3.282-2.858 9.936 9.936 0 0 0 1.75-3.97 19.615 19.615 0 0 1-2.845 2.216Z"/>
+                        </svg>
+                        <svg className="w-9 h-9 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                            <path d="M11.782 5.72a4.773 4.773 0 0 0-4.8 4.173 3.43 3.43 0 0 1 2.741-1.687c1.689 0 2.974 1.972 3.758 2.587a5.733 5.733 0 0 0 5.382.935c2-.638 2.934-2.865 3.137-3.921-.969 1.379-2.44 2.207-4.259 1.231-1.253-.673-2.19-3.438-5.959-3.318ZM6.8 11.979A4.772 4.772 0 0 0 2 16.151a3.431 3.431 0 0 1 2.745-1.687c1.689 0 2.974 1.972 3.758 2.587a5.733 5.733 0 0 0 5.382.935c2-.638 2.933-2.865 3.137-3.921-.97 1.379-2.44 2.208-4.259 1.231-1.253-.673-2.19-3.443-5.963-3.317Z"/>
+                        </svg>
+                    </div>
+                    <div className="flex justify-center">
+                        <a href="https://github.com/EzraArya/portofolio" className="flex flex-row gap-2 border rounded-xl w-40 h-9 justify-center items-center hover:bg-gray-200 drop-shadow-md">
+                              <svg className="w-5 h-5 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                              <path fill-rule="evenodd" d="M12.006 2a9.847 9.847 0 0 0-6.484 2.44 10.32 10.32 0 0 0-3.393 6.17 10.48 10.48 0 0 0 1.317 6.955 10.045 10.045 0 0 0 5.4 4.418c.504.095.683-.223.683-.494 0-.245-.01-1.052-.014-1.908-2.78.62-3.366-1.21-3.366-1.21a2.711 2.711 0 0 0-1.11-1.5c-.907-.637.07-.621.07-.621.317.044.62.163.885.346.266.183.487.426.647.71.135.253.318.476.538.655a2.079 2.079 0 0 0 2.37.196c.045-.52.27-1.006.635-1.37-2.219-.259-4.554-1.138-4.554-5.07a4.022 4.022 0 0 1 1.031-2.75 3.77 3.77 0 0 1 .096-2.713s.839-.275 2.749 1.05a9.26 9.26 0 0 1 5.004 0c1.906-1.325 2.74-1.05 2.74-1.05.37.858.406 1.828.101 2.713a4.017 4.017 0 0 1 1.029 2.75c0 3.939-2.339 4.805-4.564 5.058a2.471 2.471 0 0 1 .679 1.897c0 1.372-.012 2.477-.012 2.814 0 .272.18.592.687.492a10.05 10.05 0 0 0 5.388-4.421 10.473 10.473 0 0 0 1.313-6.948 10.32 10.32 0 0 0-3.39-6.165A9.847 9.847 0 0 0 12.007 2Z" clip-rule="evenodd"/>
+                            </svg>
+                              GitHub
+                            <svg className="w-4 h-4 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18 14v4.833A1.166 1.166 0 0 1 16.833 20H5.167A1.167 1.167 0 0 1 4 18.833V7.167A1.166 1.166 0 0 1 5.167 6h4.618m4.447-2H20v5.768m-7.889 2.121 7.778-7.778"/>
+                            </svg>
+                          </a>
+                    </div>
+                </div>
+                <img className="relative z-10 drop-shadow-xl round-md w-[30rem] h-[20rem] md:w-[40rem] md:h-[26rem] object-contain right-14" src="/image/portofolio/home.png" alt="Your Image" />
+            </div>        
+            <div className="flex m-14 flex-col">
+                <h1 className="font-montserrat text-xl font-semibold">
+                    More Screenshot below!
+                </h1>
+                <div className="flex flex-wrap justify-center items-center w-full gap-6">
+                    <img className="relative z-10  drop-shadow-xl w-[30rem] h-[20rem] md:w-[32rem] md:h-[26rem] object-contain right-14" src="/image/portofolio/home.png" alt="Your Image" />
+                    <img className="relative z-10  drop-shadow-xl w-[30rem] h-[20rem] md:w-[32rem] md:h-[26rem] object-contain right-14" src="/image/portofolio/aboutme.png" alt="Your Image" />
+                    <img className="relative z-10  drop-shadow-xl w-[30rem] h-[20rem] md:w-[32rem] md:h-[26rem] object-contain right-14" src="/image/lapangpedia/product.png" alt="Your Image" />
+                    <img className="relative z-10  drop-shadow-xl w-[30rem] h-[20rem] md:w-[32rem] md:h-[26rem] object-contain right-14" src="/image/lapangpedia/home.png" alt="Your Image" />
+        
+                </div>
+            </div>
+        </main>
+    )
+}
